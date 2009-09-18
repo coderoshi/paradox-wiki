709f77b06b4fa9ebc4341b9dcf6e40a1900934d0
Eric Redmond <eredmond@coderoshi.local>
2009-09-18 00:38:35 -0400

    make all unknown paths as base urls

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,8 @@ ActionController::Routing::Routes.draw do |map|
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   map.root :controller => 'articles'
 
+  map.root_article ':id', :controller => 'articles', :action => 'show'
+
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
